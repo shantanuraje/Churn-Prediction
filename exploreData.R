d869674a8fdf29c7fe5303a69e7a1efceea6230c
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-11-09 17:20:50 -0500

    import library lattice for graphs test scatter plots for day, evening, night, international call attributes, test bar plots for intl/voicemail plan vs churn

--- exploreData.R
+++ exploreData.R
@@ -1,31 +1,43 @@
+library(lattice)
 dataset = read.csv("./churn_tel.csv")
 
 #Data exploration
-noOfRecords = nrow(dataset)
-noOfFeatures = ncol(dataset);
 
-#cat("Number of Records =",noOfRecords,"\n");
-#cat("Number of Featues =",noOfFeatures,"\n");
+# Scatter plots for days
+#plot(dataset$Day.Mins,dataset$Day.Calls) 
+#plot(dataset$Day.Calls,dataset$Day.Charge) 
+#plot(dataset$Day.Mins,dataset$Day.Charge)
 
-#Print get dataset information
-str(dataset)
-datasetSummary = summary(dataset$State)
-#print(datasetSummary)
-#print(unique(dataset$State))
-#for (i in dataset$State) {
-#  print(i)
-#}
-# for(i in 1:ncol(dataset)) {
-#   test = dataset[,i]
-#   break()
-  
-  # do stuff with row
-#}
-#sort(state.abb) == sort(unique(dataset$State))
-main = sort(state.abb)
-test = sort(unique(dataset$State))
-# print(typeof(main[1]))
-# print(typeof(test[1]))
-# print(int(main[1]))
-print(test[1])
-#table(factor(dataset$State)) #count number of records for each state
+# Scatter plots for evenings
+#plot(dataset$Eve.Mins,dataset$Eve.Calls) 
+#plot(dataset$Eve.Calls,dataset$Eve.Charge) 
+#plot(dataset$Eve.Mins,dataset$Eve.Charge)
+
+# Scatter plots for nights
+#plot(dataset$Night.Mins,dataset$Night.Calls) 
+#plot(dataset$Night.Calls,dataset$Night.Charge) 
+#plot(dataset$Night.Mins,dataset$Night.Charge)
+
+# Scatter plots for international service
+#plot(dataset$Intl.Mins ,dataset$Intl.Calls) 
+#plot(dataset$Intl.Calls,dataset$Intl.Charge) 
+#plot(dataset$Intl.Mins,dataset$Intl.Charge)
+
+#scatterplot matrix for days
+splom(dataset[c(8,9,10)])
+#scatterplot matrix for evenings
+splom(dataset[c(11,12,13)])
+#scatterplot matrix for nights
+splom(dataset[c(14,15,16)])
+#scatterplot matrix for international service
+splom(dataset[c(17,18,19)])
+# perform regression analysis on above categories, remove perfectly correlated linear (redundant variables)
+
+#plot international plan vs churn
+plot(dataset$Int.l.Plan,dataset$Churn.)
+
+#plot voicemail plan vs churn
+plot(dataset$VMail.Plan,dataset$Churn.)
+
+#plot international plan vs churn
+#plot(dataset$CustServ.Calls,dataset$Churn.)
