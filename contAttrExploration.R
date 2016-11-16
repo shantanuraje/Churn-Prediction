79a3710d2a63010812a5c60f8a84b8ca92a048cd
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-11-15 19:07:05 -0500

    moved categorical data exploration code to respective parts in contAttrExp code

--- contAttrExploration.R
+++ contAttrExploration.R
@@ -112,6 +112,14 @@ t.test(churn$Eve.Calls ~ churn$Churn)
 t.test(churn$Eve.Mins ~ churn$Churn)
 #retain Eve minutes
 
+#Day min Vs Eve min
+#conclusion: Higher the day min and evening min, more the churn
+qplot(churn$Eve.Mins,churn$Day.Mins,
+      data = churn,
+      colour = churn$Churn., xlab = "Evening Minutes",
+      ylab= "Day Minutes")
+
+
 hist(
   churn$Night.Mins,
   border = "blue",
@@ -224,6 +232,90 @@ plot(churnScatter3)
 #function of other 4 variables
 cor(churnScatter3[sapply(churnScatter2, is.numeric)])
 
+#Intl Plan
+
+#table for counts of Churn and International Plan
+countsIntlPlan <- table(churn$Churn,
+                        churn$Int.l.Plan,
+                        dnn = c("Churn", "International Plan"))
+
+
+#Pie chart wich shows that people who have international plan may churn
+slices <- c(countsIntlPlan[1, 2] , countsIntlPlan[2, 2])
+lbls <- c("churn: False", "churn: True")
+pct <- round(slices / sum(slices) * 100)
+lbls <- paste(lbls, pct) # add percents to labels
+lbls <- paste(lbls, "%", sep = "") # ad % to labels
+pie(slices,
+    labels = lbls,
+    col = rainbow(length(lbls)),
+    main = "People Having International Plan")
+
+
+
+#Overlayed bar chart
+barplot(
+  countsIntlPlan,
+  legend = rownames(countsIntlPlan),
+  col = c("blue", "red"),
+  ylim = c(0, 3300),
+  ylab = "Count",
+  xlab = "International Plan",
+  main = "Comparison Bar Chart:
+  Churn Proportions by International Plan"
+)
+box(which = "plot",
+    lty = "solid",
+    col = "black")
+
+
+
+#Clustered Bar Chart of Churn and Intl Plan with legend
+barplot(
+  t(countsIntlPlan),
+  col = c("blue", "green"),
+  ylim = c(0, 3300),
+  ylab = "Counts",
+  xlab = "Churn",
+  main = "International Plan Count by Churn",
+  beside = TRUE
+)
+legend(
+  "topright",
+  c(rownames(countsIntlPlan)),
+  col = c("blue", "green"),
+  pch = 15,
+  title = "Intl Plan"
+)
+box(which = "plot",
+    lty = "solid",
+    col = "black")
+
+#Vmail Plan
+#weak evidence, but still vmail plan may be predictive
+#'cause we can see int row.margin[2,1] and row.margin[2,2]
+#that the people who dont have the vmail plan and will churn % = 84
+#have vmail and will churn % = 16
+
+countsVmailPlan <- table(churn$Churn, churn$VMail.Plan,
+                         dnn = c("Churn", "Vmail Plan"))
+
+
+row.margin <- round(prop.table(countsVmailPlan, margin = 1), 4)*100
+row.margin
+
+#Vmail message's histogram gives us a spike
+#For the analysis we say that If Voice Mail Messages > 0 then VoiceMailMessages_Flag = 1;
+#otherwise VoiceMailMessages_Flag = 0
+#it reveals that it is similar to the vmal plan, hence we can eliminate vmail message
+
+churn$flag[churn$VMail.Message>0] <- 1
+churn$flag[churn$VMail.Message<=0] <- 0
+table(churn$flag,churn$Churn)
+
+
+#---------------------Graphical Evidence to retain ------------------------------
+#---------------------above variables(Customer Service Call)---------------------
 hist(
   churn$CustServ.Calls,
   border = "blue",
@@ -232,9 +324,6 @@ hist(
   xlab = "Customer Service Calls"
 )
 
-
-#---------------------Graphical Evidence to retain ------------------------------
-#---------------------above variables(Customer Service Call)---------------------
 #Customer Service Calls vs Churn
 
 ggplot() +
@@ -248,4 +337,15 @@ ggplot() +
   scale_y_continuous("Percent") +
   guides(fill = guide_legend(title = "Churn")) +
   scale_fill_manual(values = c("green", "red"))
+
+
+#------------------Multivariate relationships-------------------------------------
+
+#cust serv calls vs day calls
+#Conclusion: hiher the
+qplot(churn$Day.Mins,
+      churn$CustServ.Calls,
+      data = churn,
+      colour = churn$Churn.)
+
 #Conclusion: Customer Service Calls is predictive of churn
